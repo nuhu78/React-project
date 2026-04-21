aadc3d2c505b0571acf014934c743ef18fe8f477
nuhu78 <22-48664-3@student.aiub.edu>
2026-04-21 21:18:37 +0600

    Add wind icon to assets for weather app

--- weather-app/src/components/weather.jsx
+++ weather-app/src/components/weather.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './weather.css'
 import searchicon from '../assets/search.png'
+import sunnyicon from '../assets/sunny.png'
+import humidityicon from '../assets/humidity.png'
+import windicon from '../assets/wind.png'
 
 function Weather() {
   return (
@@ -10,7 +13,30 @@ function Weather() {
         <input type="text" placeholder="Enter city name..." />
         <img src={searchicon} alt="Search" className='search-icon' />
       </div>
+
+      {/*weather info*/}
+      <img src={sunnyicon} alt="condition" className='weather-icon' />
+      <h1 className='temperature'>25°C</h1>
+      <h2 className='location'>New York, USA</h2>
+      <p className='condition'>Sunny</p>
+      <p className='feels-like'>Feels like 28°C</p>
+
+{/*additional info*/}
+      <div className='additional-info'>
+        <div className='col'>
+          <img src={humidityicon} alt="Humidity" className='small-icon' />
+          <p>Humidity</p>
+          <p>60%</p>
+        </div>
+        <div className='col'>
+          <img src={windicon} alt="Wind" className='small-icon' />
+          <p>Wind</p>
+          <p>15 km/h</p>
+        </div>
+       
+      </div>
     </div>
+     
   )
 }
 
